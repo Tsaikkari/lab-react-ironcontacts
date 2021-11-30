f68cb2c6a4f495b0aab906213146dec1df7e8cff
Tsaikkari <kirsi.trospe@gmail.com>
2021-11-30 17:07:58 +0100

    style iteration 1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import { useState } from 'react'
-import "./App.css";
+import './App.css'
 import list from './contacts.json'
 
 function App() {
   const first5 = list.slice(0, 6)
   const [contacts, setContacts] = useState(first5)
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-        </tr>
-      </thead>
-      <tbody>
-      {contacts.map(({ pictureUrl, name, popularity, id }) => 
-        <tr key={id}>
-          <td><img src={pictureUrl} alt={name} /></td>
-          <td>{name}</td>
-          <td>{popularity}</td>
-        </tr>
-      )}
-      </tbody>
-    </table>
+    <div className='contacts-page'>
+      <h1>IronContacts</h1>
+      <div className='table'>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {contacts.map(({ pictureUrl, name, popularity, id }) => (
+              <tr key={id}>
+                <td>
+                  <img src={pictureUrl} alt={name} />
+                </td>
+                <td>{name}</td>
+                <td>{popularity.toFixed(2)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
-export default App;
-
+export default App
